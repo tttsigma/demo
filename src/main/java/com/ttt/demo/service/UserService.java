3d7918526c6af8da4e66de8df2f8daafef95abdd
TTTTTTzz <ttttttending@gmail.com>
2020-07-09 16:20:24 +0800

    fix bug

--- src/main/java/com/ttt/demo/service/UserService.java
+++ src/main/java/com/ttt/demo/service/UserService.java
@@ -12,27 +12,30 @@ public class UserService {
     @Autowired
     UserDAO userDAO;
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userDAO.findAll();
     }
 
-    public User getByNameAndPassword(String name,String password){
-        return userDAO.findAllByNameAndPassword(name,password);
+    public User getByNameAndPassword(String name, String password) {
+        return userDAO.findAllByNameAndPassword(name, password);
     }
 
-    public User getById(int id){
+    public User getById(int id) {
         return userDAO.findById(id);
     }
 
-    public void add(User user){
+    public void add(User user) {
         userDAO.save(user);
     }
 
-    public void delete(int id){
+    public void delete(int id) {
         userDAO.delete(getById(id));
     }
 
-    public void update(User bean){
+    public void update(User bean) {
+        if (bean.getPassword().isEmpty()) {
+            bean.setPassword(userDAO.getOne(bean.getId()).getPassword());
+        }
         userDAO.save(bean);
     }
 }
